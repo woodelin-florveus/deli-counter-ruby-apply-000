108831c94d71f39c836f72efe9a8e61e016e5e3f
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:34:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -23,6 +23,6 @@ def now_serving(katz_deli)
     puts "There is nobody waiting to be served!"
   else
     katz_deli.shift
-    puts "Currently serving Logan."
+    puts "Currently serving #{katz_deli}."
   end
 end
