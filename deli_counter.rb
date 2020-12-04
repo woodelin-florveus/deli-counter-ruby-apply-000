2fb99b4396d0740af3e7c2508a5e53b407a4843d
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:11:22 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -11,7 +11,7 @@ if katz_deli.empty?
   puts "The line is currently empty."
 else
   katz_deli.each_with_index do |name, num|
-    puts "The line is currently: #{num}. #{name}"
+    puts "The line is currently: #{num + 1}. #{name}"
   end
 end
 
