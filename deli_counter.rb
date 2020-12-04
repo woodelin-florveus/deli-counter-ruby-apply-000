4db5a3d957de38c6d4345dc71e5df338b39c448b
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:01:37 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,10 @@
 def line(katz_deli)
   binding.pry
-
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    # shows everyone their current place in the line
+  end
 end
 
 def take_a_number(katz_deli, name)
