697615e84b68d9295c168e166bad1943586f46c4
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:28:27 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,5 +17,7 @@ def take_a_number(katz_deli, name)
   if katz_deli.empty?
     katz_deli << name
     puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
+  else
+    "name".unshift
   end
 end
