8abc7f0d844c1e31beeafc5f75e42faa26a5b29d
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:26:34 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -17,6 +17,7 @@ def take_a_number(katz_deli, name)
 
   if katz_deli.empty?
     katz_deli << name
+    puts "Welcome, #{name}. You are number #{num} in line."
   end
 
 katz_deli.each_with_index do |person, num|
