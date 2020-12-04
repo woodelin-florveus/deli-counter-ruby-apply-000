06825ce75830476acdab9d5fa31a8dc8f877b26c
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-12-04 00:06:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,6 +4,11 @@ def line(katz_deli)
   else
     puts "The line is currently: #{katz_deli}"
   end
+
+katz_deli.each_with_index do |name, num|
+  puts "The line is currently: #{num}. #{name}"
+end
+  
 end
 
 def take_a_number(katz_deli, name)
